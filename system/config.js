bee6bb9ee7e404356f97dbf9d3965c02a6007a46
Senith-udaneth <senithudaneth025@gmail.com>
2025-07-08 07:12:42 +0530

    Add files via upload

---
+++ system/config.js
@@ -0,0 +1,12 @@
+//========HELO FRIEND========//
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6285748369114"] 
+global.namabot = 'Kurapika'
+//======================
+global.mess = { 
+owner: '*⚠︎ KHUSUS OWNER!☄️',
+premium: '*ANDA SIAPA? MINIMAL BELI PREMIUM DULU😂*',
+succes: '*DONE NIH BANG🌠*'
+}
+//======================
